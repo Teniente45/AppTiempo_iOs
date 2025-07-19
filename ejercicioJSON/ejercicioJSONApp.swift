71c5acd208a96bef47843e0e469d63c5c8c6ecef
juanlopezmarin <Juanteniente45@gmail.com>
2025-07-19 14:06:27 +0200

    Initial Commit

---
+++ ejercicioJSON/ejercicioJSONApp.swift
@@ -0,0 +1,32 @@
+//
+//  ejercicioJSONApp.swift
+//  ejercicioJSON
+//
+//  Created by Juan López Marín on 19/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ejercicioJSONApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ejercicioJSONTests/ejercicioJSONTests.swift
@@ -0,0 +1,17 @@
+//
+//  ejercicioJSONTests.swift
+//  ejercicioJSONTests
+//
+//  Created by Juan López Marín on 19/7/25.
+//
+
+import Testing
+@testable import ejercicioJSON
+
+struct ejercicioJSONTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
